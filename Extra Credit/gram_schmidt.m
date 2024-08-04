176b4ca7c74220899d6190ab5fb048e9beb67ce2
Natalie Poche <nataliealena03@gmail.com>
2024-08-04 00:27:35 -0400

    Adjusted extra credit assignment.

--- Extra Credit/gram_schmidt.m
+++ Extra Credit/gram_schmidt.m
@@ -1,8 +1,8 @@
 function [V, name, ufid] = gramm_schmidt(X, normalize)
-    % Purpose: (COMMENT)
-    % Input Argument [X]: (COMMENT)
-    % Input Argument [normalize = false]: (COMMENT)
-    % Output Argument [V]: (COMMENT)
+    % Purpose: Perform the Gram-Schmidt process to convert a basis into an orthogonal or orthonormal basis. (COMMENT)
+    % Input Argument [X]: A matrix whose columns are vectors forming a basis of some subspace. (COMMENT)
+    % Input Argument [normalize = false]: A boolean flag to indicate whether to normalize the basis vectors. (COMMENT)
+    % Output Argument [V]: A matrix whose columns are the orthogonal (or orthonormal) basis vectors. (COMMENT)
 
     % --- Name & UFID --- %
     name = "Name";
@@ -16,4 +16,24 @@ function [V, name, ufid] = gramm_schmidt(X, normalize)
     V = zeros(n, p);
 
     % (CODE BELOW)
+
+    for i = 1:p
+        % Start with the original vector
+        v = X(:, i);
+        
+        % Subtract the projection of v onto each of the previously computed vectors
+        for j = 1:i-1
+            v = v - proj(v, V(:, j));
+        end
+        
+        % Store the orthogonal vector in V
+        V(:, i) = v;
+    end
+    
+    if normalize
+        % Normalize the vectors to create an orthonormal basis
+        for i = 1:p
+            V(:, i) = V(:, i) / norm(V(:, i));
+        end
+    end
 end
